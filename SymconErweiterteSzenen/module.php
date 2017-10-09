35de8e5b407ff07211c659e6bf991fb7b5333980
DavidCks <david.christ99@live.de>
2017-10-09 11:46:22 +0200

    Update module.php

--- SymconErweiterteSzenen/module.php
+++ SymconErweiterteSzenen/module.php
@@ -117,6 +117,8 @@ class ErweiterteSzenenSteuerung extends IPSModule {
 
 		if($data != "" && $c != $cnt /*if not update but scene added*/)
 		{
+			IPS_LogMessage("new scene","s");
+			return;
 			IPS_SetPosition($this->InstanceID, -700);
 
 			//copy Values of the Sets and Selector Profile
